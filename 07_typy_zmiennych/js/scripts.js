658f1f93bd0f2fbb425bafe308e9e4382f5751e4
dominikgrabinski <grabinski.dominik@gmail.com>
2017-06-22 20:57:47 +0200

    true/false stmt

--- 07_typy_zmiennych/js/scripts.js
+++ 07_typy_zmiennych/js/scripts.js
@@ -3,3 +3,11 @@ var lastName = 'Grabiński';
 var bookTitle = "\"Łysek\"";
 
 var multiline = "Linijka pierwsza\nlinijka druga\n\ttrzecialinijka";
+
+var number = 20;
+
+var price = 20.5;
+
+var inf = 'Infinity';
+
+
---
+++ 09_true_false/js/scripts.js
@@ -0,0 +1,13 @@
+var isLoggedIn = true;
+
+if(isLoggedIn) {
+    console.log("Zalogowano");
+} else {
+    console.log("nie zalogowano");
+}
+
+var value = "0";
+
+if (value) {
+    console.log("Ta wartość nie jest fałszywa");
+}
