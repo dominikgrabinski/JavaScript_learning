393ca2aa256e6969cd88f2d3f26bd5a47c4562c1
dominikgrabinski <grabinski.dominik@gmail.com>
2017-07-07 08:40:08 +0200

    compare operator

---
+++ 12_operatory_porownania/js/scripts.js
@@ -0,0 +1,32 @@
+var number = 20;
+
+ if(number == 20) {
+     console.log("Liczba jest równa 20");
+ }
+
+ if(false === 0) {
+     console.log("Undefined jest równe null");
+ }
+
+if(number !== "20") {
+    console.log("Liczba nie jest równa 20");
+}
+
+
+if(number > 10) {
+console.log("Liczba jest większa od 10");
+}
+if(number < 10) {
+    console.log("Liczba jest mniejsza od 10");
+}
+
+if(number >= 20) {
+    console.log("Liczba jest większa bądź równa od 10");
+}
+
+if(number <= 20) {
+    console.log("Liczba jest mniejsza bądź równa od 10");
+}
+
+var number2 = 6;
+var isEven = (number % 2) === 0;
