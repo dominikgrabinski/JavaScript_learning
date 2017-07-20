e16e9fb8fdb63f9fa3ffe330e1eb73fc024d13c5
dominikgrabinski <grabinski.dominik@gmail.com>
2017-07-20 10:23:08 +0200

    logical_operators

---
+++ 13_operatory_logiczne/js/scripts.js
@@ -0,0 +1,16 @@
+var number = 10;
+
+console.log("XYZ" && "abc");
+
+var firstName = "Jan",
+    lastName = "Grabiński";
+
+if(firstName === "Dominik" || lastName === "Grabiński") {
+  console.log("Jacek lub Nowak");
+}
+
+var name = "Andrzej";
+
+if(!name){
+  console.log("Imię nie zostało podane!");
+}
