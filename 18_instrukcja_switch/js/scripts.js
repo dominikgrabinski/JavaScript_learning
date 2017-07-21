6a130ce07a63215d0298370eea62fbb68da51cda
dominikgrabinski <grabinski.dominik@gmail.com>
2017-07-21 19:01:55 +0200

    switch

---
+++ 18_instrukcja_switch/js/scripts.js
@@ -0,0 +1,29 @@
+var number = 3,
+    word = "";
+
+// if (number === 1) {
+//   word = "jeden";
+// } else if (number === 2) {
+//   word = "dwa";
+// } else if (number === 3) {
+//   word = "trzy";
+// } else {
+//   word = "wartość nieznana";
+// }
+
+switch (number) {
+  case 1:
+    word = "jeden";
+    break;
+  case 2:
+    word = "dwa";
+    break;
+  case 3:
+    word = "trzy";
+    break;
+  default:
+    word = "Wartość nieznana";
+
+}
+
+console.log("Wartość to " + word + ".");
