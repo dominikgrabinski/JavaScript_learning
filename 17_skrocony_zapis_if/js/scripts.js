f806f8a2e6e01e919a5cd565ae7c20e3313db0dd
dominikgrabinski <grabinski.dominik@gmail.com>
2017-07-21 19:01:15 +0200

    short_if

---
+++ 17_skrocony_zapis_if/js/scripts.js
@@ -0,0 +1,13 @@
+var firstName = "Axa";
+
+var userName;
+
+// if (firstName) {
+//   userName = firstName;
+// } else {
+//   userName = "Niezajomy";
+// }
+
+userName = firstName ? firstName : "Niezajomy";
+
+console.log("Witaj, " + userName);
