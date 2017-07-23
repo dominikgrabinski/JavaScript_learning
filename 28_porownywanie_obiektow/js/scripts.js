6e7da85bc1266586025e643d5985904dceb9622f
dominikgrabinski <grabinski.dominik@gmail.com>
2017-07-23 11:50:02 +0200

    compare_objects

---
+++ 28_porownywanie_obiektow/js/scripts.js
@@ -0,0 +1,13 @@
+var person = {
+  firstName: "Axa",
+  lastName: "Siekiera"
+};
+
+var person2 = {
+  firstName: "Axa",
+  lastName: "Siekiera"
+};
+
+var person3 = person2;
+
+console.log(person3 === person2);
