ead123ad4c70425cf4afb65a334f1cc056829d51
dominikgrabinski <grabinski.dominik@gmail.com>
2017-07-25 10:51:56 +0200

    object_number

---
+++ 45_obiekt_number/js/scripts.js
@@ -0,0 +1,17 @@
+var number = 23.158;
+
+var number2 = Number("23");
+
+//console.log( number2 );
+
+console.log( Number.MAX_VALUE );
+console.log( Number.MIN_VALUE );
+console.log( Number.NaN );
+console.log( Number.NEGATIVE_INFINITY );
+console.log( Number.POSITIVE_INFINITY );
+
+console.log( Infinity === Number.POSITIVE_INFINITY );
+
+console.log( number.toString(16) );
+
+console.log( number.toFixed(2) );
