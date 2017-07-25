998cdd743795806a42d4ef68b33d1ce59336c9fd
dominikgrabinski <grabinski.dominik@gmail.com>
2017-07-25 10:50:16 +0200

    JS_inside_functions

---
+++ 43_wbudowane_funkcje_JS/js/scripts.js
@@ -0,0 +1,6 @@
+//eval('alert("Witaj")');
+//console.log( isFinite(-Infinity) );
+// console.log( !isNaN(2 * undefined) );
+//console.log( parseFloat("20.5") );
+//console.log( parseInt("20zł") );
+alert("Witaj");
