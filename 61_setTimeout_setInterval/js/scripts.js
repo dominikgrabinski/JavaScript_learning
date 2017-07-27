01f7f0ff580e8ce7172b90eba54f456c45e44192
dominikgrabinski <grabinski.dominik@gmail.com>
2017-07-27 08:54:44 +0200

    setTimeout_setInterval

---
+++ 61_setTimeout_setInterval/js/scripts.js
@@ -0,0 +1,34 @@
+// setTimeout( function() {
+//   console.log("Witaj po 2 sekundach");
+// }, 2000);
+
+var timeout = setTimeout( function() {
+  console.log("Witaj po 5 sekundach");
+}, 5000);
+
+clearTimeout(timeout);
+
+var counter = 0,
+clock = null;
+
+function tick() {
+
+  if(counter <= 5) {
+    console.log(counter++);
+  } else {
+    clearInterval(clock);
+  }
+}
+
+//clock = setInterval(tick, 1000);
+
+counter = 0;
+
+function tock() {
+  if(counter <= 5) {
+    console.log(counter++);
+    setTimeout(tock, 1000);
+  }
+}
+
+tock();
