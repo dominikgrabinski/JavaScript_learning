050074276cfbde615a9b58c1c7ef68137453a8f5
dominikgrabinski <grabinski.dominik@gmail.com>
2017-07-27 08:51:45 +0200

    window_global>object

---
+++ 57_globalny_obiekt_window/js/scripts.js
@@ -0,0 +1,19 @@
+// console.log(window);
+function sayHello() {
+
+  message = "Cześć!";
+
+  //alert(message);
+  //var con = confirm("Czy wyrażasz zgodę?");
+  var ans = prompt("Jak masz na imię?" ,"Podaj swoje imię ?");
+
+
+  console.log(ans);
+
+//  window.myObject = {};
+
+}
+
+sayHello();
+
+console.log(message);
